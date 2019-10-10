dabdf87e840fc58f80c61a33ca040b1f0c70ca1e
vasuram <vasu_ram@yahoo.com>
2019-10-10 13:22:58 -0500

    Update studentload.sh

--- studentload.sh
+++ studentload.sh
@@ -1,3 +1,3 @@
 create table student ( id int not null, name text not null,rollnumber int not null);
-\copy student (id,name,rollnumber) from '/Users/vasuram/ansible/roles/postgresql/student.csv' delimiters ',' csv ;
+\copy student (id,name,rollnumber) from '/home/centos/student.csv' delimiters ',' csv ;
 select * from student;
